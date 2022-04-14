e41384b92770fee09cca8a578d7db88ac391b839
Muhammad Asim <muhammadasim4515@gmail.com>
2022-04-14 15:41:14 +0500

    monogo db uri bug fix

--- models/conn.js
+++ models/conn.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
-const uri =
-  process.env.MONGODB_URI ||
-  "mongodb+srv://asim:1234@cluster0.3jjse.mongodb.net/ecommerce?retryWrites=true&w=majority";
+const uri = process.env.MONGODB_URI;
+// || "mongodb+srv://asim:1234@cluster0.3jjse.mongodb.net/ecommerce?retryWrites=true&w=majority";
 mongoose
   .connect(uri)
   .then(() => {
